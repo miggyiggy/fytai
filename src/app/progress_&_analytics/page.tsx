dfc11a4892c2ca14da5a8d8d99a0926458bcff44
miggyiggy <juanmolina@mymail.mapua.edu.ph>
2025-03-15 00:13:34 +0800

    minor changes

--- src/app/progress_&_analytics/page.tsx
+++ src/app/progress_&_analytics/page.tsx
@@ -1,1 +1,30 @@
-a
+"use client";
+import React from "react";
+import styled from "styled-components";
+import NavigationBar from "../fcomp/NavigationBar";
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const ContentContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+`;
+
+const ProgressAnalytics = () => {
+    return(
+        <PageContainer>
+            <NavigationBar />
+            <ContentContainer>
+            </ContentContainer>
+        </PageContainer>
+    )
+}
+
+export default ProgressAnalytics;
--- src/app/workout_planner/page.tsx
+++ src/app/workout_planner/page.tsx
@@ -1,1 +1,29 @@
-a
+"use client";
+import React from "react";
+import styled from "styled-components";
+import NavigationBar from "../fcomp/NavigationBar";
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const ContentContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+`;
+
+const WorkoutPlanner = () => {
+    return(
+        <PageContainer>
+            <NavigationBar />
+            <ContentContainer>
+                
+            </ContentContainer>
+        </PageContainer>
+    )
+}
